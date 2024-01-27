9b4a6afa3dab5c5e4169c498f935af36b59453c0
miguel angel perez <92065472+lord-red-anubis@users.noreply.github.com>
2024-01-27 12:02:22 -0600

    Add files via upload

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Imprime "Hola Mundo" en la consola de Unity.
+        Debug.Log("Hola Mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
